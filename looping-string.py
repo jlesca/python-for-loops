eb4dc9c793450e9f49b2c4817054be6a955eb4db
José Lescano <92216756+jlesca@users.noreply.github.com>
2022-05-15 12:36:35 -0300

    Create looping-string.py

---
+++ looping-string.py
@@ -0,0 +1,8 @@
+# Strings are a iterable objects in Python
+
+# Create a variable and set a string value
+color = "yellow"
+
+# Execute a FOR function
+for x in color:
+  print(x) # It'll display each character as our variable contain 
